2a922ccc30e560d01a9d984bd43e4ad72df5fb7f
Michael Ip <michaelip@Michaels-Air.home>
2020-02-02 11:20:06 -0500

    created basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header (){
+    return (
+        <div>
+            <h1 className="headername">SimpleStock View</h1>
+        </div>
+    )
+}
+
+export default Header;
